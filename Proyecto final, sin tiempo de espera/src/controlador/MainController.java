60c593838e415e7e40caea52902f305864425133
arturograu <argrau3@gmail.com>
2015-05-18 20:15:15 +0200

    Otra prueba
    Signed-off-by: arturograu <argrau3@gmail.com>

--- Proyecto final, sin tiempo de espera/src/controlador/MainController.java
+++ Proyecto final, sin tiempo de espera/src/controlador/MainController.java
@@ -3,6 +3,7 @@ package controlador;
 public class MainController 
 {
 	//Nuevo test de subir y bajar cosas -.-"
+	//Se ha bajado bien, ahora te toca ti
 	public MainController() 
 	{
 		// TODO Auto-generated constructor stub
